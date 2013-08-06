23a9e26a5837a5fcf573675284702bcbfabd6186
Bob Liu <bob.liu@oracle.com>
2013-08-06 19:36:17 +0800

    mm: add WasActive page flag
    Zcache could be ineffective if the compressed memory pool is full with
    compressed inactive file pages and most of them will be never used again.
    
    So we pick up pages from active file list only, those pages would probably
    be accessed again. Compress them in memory can reduce the latency
    significantly compared with rereading from disk.
    
    When a file page is shrunk from active file list to inactive file list,
    PageActive flag is also cleared.
    So adding an extra WasActive page flag for zcache to know whether the
    file page was shrunk from the active list.
    
    Change-Id: Ida1f4db17075d1f6f825ef7ce2b3bae4eb799e3f
    Signed-off-by: Bob Liu <bob.liu@oracle.com>
    Patch-mainline: linux-mm @ 2013-08-06 11:36:17
    [vinmenon@codeaurora.org: trivial merge conflict fixes, checkpatch fixes,
    fix the definitions of was_active page flag so that it does not create
    compile time errors with CONFIG_CLEANCACHE disabled. Also remove the
    unnecessary use of PG_was_active in PAGE_FLAGS_CHECK_AT_PREP. Since
    was_active is a requirement for zcache, make the definitions dependent on
    CONFIG_ZCACHE rather than CONFIG_CLEANCACHE.]
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Signed-off-by: AayushRd7 <aayush835@gmail.com>
    
    Conflicts:
    	include/linux/page-flags.h

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -113,6 +113,9 @@ enum pageflags {
 	PG_ksm_scan0,		/* page has been scanned by even KSM cycle */
 #endif
 	PG_readahead,		/* page in a readahead window */
+#ifdef CONFIG_ZCACHE
+	PG_was_active,
+#endif
 	__NR_PAGEFLAGS,
 
 #ifdef CONFIG_KSM_CHECK_PAGE
@@ -218,9 +221,10 @@ PAGEFLAG(Reserved, reserved) __CLEARPAGEFLAG(Reserved, reserved)
 PAGEFLAG(SwapBacked, swapbacked) __CLEARPAGEFLAG(SwapBacked, swapbacked)
 
 __PAGEFLAG(SlobFree, slob_free)
-#ifdef CONFIG_KSM_CHECK_PAGE
-CLEARPAGEFLAG(KsmScan0, ksm_scan0) TESTSETFLAG(KsmScan0, ksm_scan0)
-CLEARPAGEFLAG(KsmScan1, ksm_scan1) TESTSETFLAG(KsmScan1, ksm_scan1)
+#ifdef CONFIG_ZCACHE
+PAGEFLAG(WasActive, was_active)
+#else
+PAGEFLAG_FALSE(WasActive)
 #endif
 
 /*
