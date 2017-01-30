0e003fafb700d0395e925603dba8be9b7ed30de9
AayushRd7 <aayush835@gmail.com>
2017-01-30 17:31:25 +0530

    Make adreno_idler stable for arm64
    Signed-off-by: AayushRd7 <aayush835@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -385,8 +385,8 @@ static int tz_resume(struct devfreq *devfreq)
 static int tz_suspend(struct devfreq *devfreq)
 {
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
-	struct devfreq_dev_profile *profile = devfreq->profile;
-	unsigned long freq;
+	unsigned int scm_data[2] = {0, 0};
+	__secure_tz_reset_entry2(scm_data, sizeof(scm_data), priv->is_64);
 
 	suspended = true;
 
@@ -396,9 +396,7 @@ static int tz_suspend(struct devfreq *devfreq)
 	priv->bus.gpu_time = 0;
 	priv->bus.ram_time = 0;
 
-	freq = profile->freq_table[profile->max_state - 1];
-
-	return profile->target(devfreq->dev.parent, &freq, 0);
+        return 0;
 }
 
 static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
